b03c7227b788998da951ec7d8c6a7a32ab4b96f5
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-10-28 21:44:57 +0100

    Update bot2.js

--- bot2.js
+++ bot2.js
@@ -7,19 +7,19 @@ const other = new Discord.Client();
 client.on('ready', async () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
-client.channels.get("502405448222375946").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+client.channels.get("501142112318783490").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
 },5000);
 });
 other.on('ready', async () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
-client.channels.get("502405448222375946").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+client.channels.get("501142112318783490").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
 },5000);
 });
 heros.on('ready', async () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
-client.channels.get("502405448222375946").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+client.channels.get("501142112318783490").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
 },5000);
 });
 لا تنسى تحط أيدي الروم
@@ -32,7 +32,7 @@ client.on('ready', () => {
 });
 
 
-client.login(process.env.BOT_TOKEN1);
+client.login(process.env.BOT_TOKEN);
 client.login(process.env.SPAM);
 heros.login(process.env.HER);
 other.login(process.env.OTH);
